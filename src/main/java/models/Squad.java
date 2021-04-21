f3d5fc8de035d24d92ae641424512ee33ec45cd7
KevinMokoro <klosvoke1@gmail.com>
2021-04-21 11:03:06 +0300

    Add a constructor to Squad.java and a getter method for name attribute.

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,6 +1,15 @@
 package models;
 
 public class Squad {
+    private String name;
 
 
+    public Squad(String name,int maxSize,String cause){
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +14,21 @@ public class SquadTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    public Squad setUpNewSquad() {
+        return new Squad("Fast",5,"Cause");
+    }
+
+    @Test
+    public void squadInstantiatesCorrectly_true() throws Exception {
+        Squad newSquad = setUpNewSquad();
+        assertTrue(newSquad instanceof Squad);
+    }
+
+    @Test
+    public void squadGetsNameCorrectly_String() {
+        Squad newSquad = setUpNewSquad();
+        assertEquals("Fast",newSquad.getName());
     }
 }
